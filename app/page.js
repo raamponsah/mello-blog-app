590d2b1acfe7e50bb51adb0a4072f636c7b12795
Raamponsah <raamponsah@gimpa.edu.gh>
2024-08-09 14:47:37 +0000

    pushed

--- app/page.js
+++ app/page.js
@@ -4,14 +4,10 @@ export default async function Page(){
    const articles =  await fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => {return data})
-
-      
-
   return (
     <div className="mx-40" space-y-10>
       <h1 className="text-2xl font-light">Mello Blog App</h1>
       <div className="space-y-10 mt-4">
-
      {
       articles.map((article,index)=>{
         return (
